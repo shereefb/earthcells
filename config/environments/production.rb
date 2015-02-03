537b629eda42e121da8ea15b422587ff6bcb6cd4
Shereef Bishay <shereef@gmail.com>
2015-02-03 15:55:52 -0800

    don't send emails for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Loomio::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   # Send emails using SMTP service
   config.action_mailer.delivery_method = :smtp

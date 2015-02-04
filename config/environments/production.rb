e0560592f0c3f057b3b438bc902ebf9e05974710
Shereef Bishay <shereef@gmail.com>
2015-02-03 23:31:24 -0800

    ok, now try to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Loomio::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   # Send emails using SMTP service
   config.action_mailer.delivery_method = :smtp

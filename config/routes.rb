9d2eff5fd8179e2ce3fa67e255cb9cfaf1b310e8
Shereef Bishay <shereef@gmail.com>
2015-02-04 01:59:10 -0800

    removing silly bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'gollum/app'
 
 Loomio::Application.routes.draw do
 
-  Gollum support
+  # Gollum support
   Precious::App.set(:gollum_path, Rails.root.join('wiki').to_s)
   Precious::App.set(:default_markup, :markdown) # set your favorite markup language
   Precious::App.set(:wiki_options, {:universal_toc => false})

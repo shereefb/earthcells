122838622861ab029e412f37d83d808585d3c829
Shereef Bishay <shereef@gmail.com>
2015-02-04 01:50:37 -0800

    resetting some serializer changes

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,24 +2,12 @@ class EventSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :sequence_id, :kind, :discussion_id, :created_at
 
-  has_one :actor, serializer: UserSerializer, root: 'users'
-  has_one :membership_request, serializer: MembershipRequestSerializer, root: 'membership_requests'
+
   has_one :comment, serializer: CommentSerializer
   has_one :discussion, serializer: DiscussionSerializer
   has_one :proposal, serializer: MotionSerializer, root: 'proposals'
   has_one :vote, serializer: VoteSerializer
-
-  def include_membership_request?
-    ['membership_requested', 'membership_request_approved'].include? object.kind
-  end
-
-  def membership_request
-    object.eventable
-  end
-
-  def actor
-    object.eventable.try(:user)
-  end
+  has_one :actor, serializer: UserSerializer, root: 'users'
 
   def group_kinds
     ['new_discussion', 'user_added_to_group', 'membership_request', 'membership_request_approved']

5a415de9cc19b311b4b732df0efa8b41754a43cd
Shereef Bishay <shereef@gmail.com>
2015-02-05 03:29:34 -0800

    returning avatar code

--- app/models/user.rb
+++ app/models/user.rb
@@ -319,13 +319,12 @@ class User < ActiveRecord::Base
   end
 
   def has_gravatar?(options = {})
-    return false
-    # hash = Digest::MD5.hexdigest(email.to_s.downcase)
-    # options = { :rating => 'x', :timeout => 2 }.merge(options)
-    # http = Net::HTTP.new('www.gravatar.com', 80)
-    # http.read_timeout = options[:timeout]
-    # response = http.request_head("/avatar/#{hash}?rating=#{options[:rating]}&default=http://gravatar.com/avatar")
-    # response.code != '302'
+    hash = Digest::MD5.hexdigest(email.to_s.downcase)
+    options = { :rating => 'x', :timeout => 2 }.merge(options)
+    http = Net::HTTP.new('www.gravatar.com', 80)
+    http.read_timeout = options[:timeout]
+    response = http.request_head("/avatar/#{hash}?rating=#{options[:rating]}&default=http://gravatar.com/avatar")
+    response.code != '302'
   rescue StandardError, Timeout::Error
     false  # Don't show "gravatar" if the service is down or slow
   end

5859250f1e412f4735b35a7e48527c7122979132
Shereef Bishay <shereef@gmail.com>
2015-02-05 23:50:15 -0800

    new names for subgroups

---
+++ lib/bazaar.rb
@@ -0,0 +1,28 @@
+require "bazaar/version"
+
+module Bazaar
+  def self.item
+    get_item("items").humanize
+  end
+  def self.adj
+    get_item("adj").humanize
+  end
+  def self.object
+    (get_item("adj") + ' ' + get_item("items")).humanize
+  end
+  def self.super_item
+    get_item("superitems").humanize
+  end
+  def self.super_adj
+    get_item("superadj").humanize
+  end
+  def self.name
+    (get_item("superadj") + ' ' + get_item("superitems")).humanize
+  end
+  def self.heroku
+    get_item("superadj") + '-' + get_item("superitems") + '-' + rand(0-9999).to_s
+  end
+  def self.get_item(filename)
+    File.read(File.expand_path("../bazaar/#{filename}.txt", __FILE__)).split("\n").sample
+  end
+end
